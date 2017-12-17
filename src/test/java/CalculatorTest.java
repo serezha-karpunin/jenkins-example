1472d3e510b4818bec238912b69bd05315b0481b
Sergei_Karpunin <Sergei_Karpunin@epam.com>
2017-12-17 18:54:49 +0300

    init commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void sumTest() {
+        assertEquals(3, calculator.sum(1,2));
+    }
+}
